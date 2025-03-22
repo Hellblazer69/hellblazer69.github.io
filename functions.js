43f212c666531af27faedf84f148fd9b0fa1523f
Hellblazer69 <shipxonze@gmail.com>
2025-03-22 14:56:25 +0400

    add title and fix js function

--- functions.js
+++ functions.js
@@ -16,3 +16,26 @@ document.getElementById("phone").addEventListener("input", function (e) {
         phoneError.textContent = "";
     }
 });
+
+function openTab(evt, tabName) {
+
+    let tabContents = document.getElementsByClassName("tab-content");
+    for (let content of tabContents) {
+        content.classList.remove("active");
+    }
+
+    let tabLinks = document.getElementsByClassName("tab-link");
+    for (let link of tabLinks) {
+        link.classList.remove("active");
+    }
+
+    document.getElementById(tabName).classList.add("active");
+
+    if (tabName === "formTab") {
+        document.querySelector('.nav-links a[onclick*="formTab"]').classList.add("active");
+    } else {
+        if (evt && evt.currentTarget.classList.contains("tab-link")) {
+            evt.currentTarget.classList.add("active");
+        }
+    }
+}
